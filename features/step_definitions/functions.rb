1bce085c20a178b01bf4a48715e3a59b45adac47
Dobro <matematikaigor@gmail.com>
2020-03-01 21:32:24 +0300

    remove unneccessary steps

--- features/step_definitions/functions.rb
+++ features/step_definitions/functions.rb
@@ -1,31 +1,3 @@
-def recursion(obj) # Возвращает массив [ключ, значение, ключ, значение, ...] вне зависимости от вложенности принимаемого объекта
-  result = []
-
-  def iter(counter, acc)
-    if counter.class != Hash && counter.class != Array
-      acc.push(counter)
-    elsif counter.is_a?(Hash)
-      iter(counter.flatten, acc)
-    else
-      counter.each do |value|
-        iter(value, acc)
-      end
-    end
-  end
-
-  iter(obj, result)
-  result
-end
-
-def wait_while(timeout = 600, retry_interval = 20, *args, &block)
-  start = Time.now
-  while (result = !!block.call)
-    break if (Time.now - start).to_i >= timeout
-    sleep(retry_interval)
-  end
-  !result
-end
-
 def configure_connection_to_database
   @connection = Mysql2::Client.new(:host => "remotemysql.com",
                      :username => "e1DOZmmbdj",
--- features/step_definitions/rest_steps.rb
+++ features/step_definitions/rest_steps.rb
@@ -132,14 +132,6 @@ When(/^Проверили, что status code == (\d*) или (\d*)$/) do |code1
   expect(@last_response.code.to_s).to eq(code1).or eq(code2)
 end
 
-When(/Проверили, что статус ошибки (.*) соответствует названию ошибки (.*)/) do |status_code, error|
-  arr_of_hashes = JSON.parse @last_response.body
-  errors = arr_of_hashes["errors"][0]
-  code_of_error = errors["code"]
-  status_of_error = errors["status"]
-  expect(status_code.to_i).to eq(status_of_error)
-  expect(error).to eq(code_of_error)
-end
 
 When(/^Послали GET "(.*)" запрос$/) do |url|
   @response = send_get url
@@ -147,56 +139,3 @@ When(/^Послали GET "(.*)" запрос$/) do |url|
   @last_response = @response
 end
 
-When(/Запомнили значение параметра (.*), который получили после POST запроса/) do |param|
-  arr_of_hashes = JSON.parse @last_response.body
-  @value_to_remember = arr_of_hashes["#{param}"]
-end
-
-When(/^Сверили значение полученного параметра (.*) с запомненным/) do |param|
-  step "Находим значение ключа #{param} и сравниваем с #{@value_to_remember}"
-end
-
-When(/Убедились, что мы НЕ нашли в GET запросе наше запомненное значение (.*)/) do |param|
-  path = JsonPath.new("$..#{param}")
-  arr_of_values = path.on(@last_response.body)
-  expect(arr_of_values).not_to include @value_to_remember
-end
-
-When(/Находим значение ключа (.*) и сравниваем с (.*)/) do |key, value|
-  path = JsonPath.new("$..#{key}")
-  arr_of_values = path.on(@last_response.body)
-  expect(arr_of_values).to include value
-end
-
-When(/Сверили запомненное значение со значением параметра - (.*) из SQL таблицы (.*)/) do |param, sql_table|
-  step "Инициализировали sql подключение"
-  step "Настроили параметры подключения к ALFAMOSU"
-  step "SQL: SELECT * FROM afilb01.#{sql_table} WHERE #{sql_table[0, 3].upcase}#{param.upcase} = '#{@value_to_remember}'"
-  @sql = sql_table
-  @parametr = param
-  expect(@sql_to_remember[0]["#{sql_table[0, 3].upcase}#{param.upcase}"]).to eq(@value_to_remember)
-end
-
-When(/Проверили, что поле (.*) записи в SQL таблице (.*) == (.*)/) do |name, sql_table, value|
-  expect(@sql_to_remember[0]["#{sql_table[0, 3].upcase}#{name.upcase}"]).to eq(value)
-end
-
-When(/Пытаемся скачать отчёт по запомненному (.*) пользователем с USID - (.*), ORID - (.*), у которого недостаточно прав/) do |rpid, usid, orid|
-  step "Послали GET 'http://ufrmspr1/ufr-azon-reports-api/reports/#{@value_to_remember}/download?usid=#{usid}&orid=#{orid}' запрос"
-end
-
-When(/Ждём (.*) минут, или пока поле (.*) в SQL таблице (.*) станет (.*)/) do |minute, param, sql_table, value|
-  step "Инициализировали sql подключение"
-  step "Настроили параметры подключения к ALFAMOSU"
-  wait_while(minute.to_i * 60, 10) do
-    step "SQL: SELECT * FROM afilb01.#{sql_table} WHERE #{sql_table[0, 3].upcase}#{@parametr.upcase} = '#{@value_to_remember}'"
-    @sql_to_remember[0]["#{sql_table[0, 3].upcase}#{param.upcase}"] != value
-  end
-  expect(@sql_to_remember[0]["#{sql_table[0, 3].upcase}#{param.upcase}"]).to eq(value)
-end
-
-When(/Удостоверились, что мы получили ошибку - (.*)/) do |error|
-  path = JsonPath.new("$..jexceptionMsg")
-  arr_of_values = path.on(@last_response.body)
-  expect(arr_of_values[0]).to match /.*#{error}/
-end
