fa38185cd9b858f3172d6c6e4ad398319b54bdab
Dobro <matematikaigor@gmail.com>
2020-03-02 22:59:46 +0300

    fix 'too many connections'

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,20 +1,16 @@
-# encoding: utf-8
 # frozen_string_literal: true
 
 
 Before('@rest') do |scenario|
-  close
   @counter = 0
   @scenarios_name = scenario.name
-  if ENV["DbLogEnable"] == 'true'
-    configure_connection_to_database
-  end
+  configure_connection_to_database if ENV['DbLogEnable'] == 'true'
 end
 
 AfterStep('@rest') do |_result, step|
   @counter += 1
   steps_name = step.text
-  if ENV["DbLogEnable"] == 'true'
+  if ENV['DbLogEnable'] == 'true'
     log_in_db_successful(@scenarios_name, steps_name)
   end
 end
@@ -22,21 +18,22 @@ end
 After('@rest') do |scenario|
   if scenario.failed?
     error = scenario.exception
-    arr_of_steps = scenario.test_steps.map(&:text).delete_if { |item| item.include? "hook" }
+    arr_of_steps = scenario.test_steps.map(&:text).delete_if { |item| item.include? 'hook' }
     arr_of_steps[@counter]
-    if ENV["DbLogEnable"] == 'true'
+    if ENV['DbLogEnable'] == 'true'
       log_in_db_unsuccessful(@scenarios_name, arr_of_steps[@counter], error)
     end
   end
+  @connection&.close
   # sql_SELECT
 end
 
 
 # Выполняется перед сценарием
 Before('@ui') do
-  Selenium::WebDriver::Chrome::Service.driver_path = "chromedriver.exe"
+  Selenium::WebDriver::Chrome::Service.driver_path = 'chromedriver.exe'
   absolute_path = get_filepath('features/support/utils/GoogleChromePortable/App/Chrome-bin/chrome.exe')
-  options = Selenium::WebDriver::Chrome::Options.new(binary: "#{absolute_path}")
+  options = Selenium::WebDriver::Chrome::Options.new(binary: absolute_path.to_s)
   @browser = Selenium::WebDriver.for(:chrome, options: options)
   @browser.manage.window.maximize
   set_page_timeouts
@@ -44,8 +41,6 @@ end
 
 # Выполняется после сценария
 After('@ui') do |scenario|
-  if scenario.failed?
-    screenshot
-  end
+  screenshot if scenario.failed?
   quit_browser
 end
